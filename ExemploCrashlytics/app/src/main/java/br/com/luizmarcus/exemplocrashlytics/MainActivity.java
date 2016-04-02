58ae0a845241d2adedb7fb5f82b16f0451da9553
Luiz Marcus <Luiz Marcus>
2016-04-01 21:13:55 -0300

    Exemplo Crashlytics

---
+++ ExemploCrashlytics/app/src/main/java/br/com/luizmarcus/exemplocrashlytics/MainActivity.java
@@ -0,0 +1,23 @@
+package br.com.luizmarcus.exemplocrashlytics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+    }
+
+    public void forceCrash(View view) {
+        throw new RuntimeException("Ocorreu um erro!");
+    }
+
+}
